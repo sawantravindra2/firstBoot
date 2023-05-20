0a2780fd1d7ee799997d1717f04c1951b4b3f524
sawantravindra2 <sawantravindra2@gmail.com>
2023-05-20 10:44:47 +0530

    Code Refactor

---
+++ firstBoot/src/main/java/com/ravi/dao/AddressDao.java
@@ -0,0 +1,15 @@
+package com.ravi.dao;
+
+
+import com.ravi.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AddressDao  extends JpaRepository<Address,Integer> {
+
+
+    Optional<Address> findById(Integer id);
+}
